f021597549529902ec15e9f5469d327e823c9dee
xianengqi <xianengqi@gmail.com>
2020-07-27 08:31:58 +0800

    feat:

--- src/index.js
+++ src/index.js
@@ -1,20 +1,68 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    hello: String!
+    hello(name: String): String
+    user: User
+  }
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Error {
+    field: String!
+    message: String!
+  }
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+  type RegisterResponse {
+    error: [Error!]!
+    user: User!
+  }
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): String!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'hello world!!!!'
-  }
-}
+    hello: (parent, { name }) => {
+      return `hey ${name}`;
+    },
+    user: () => ({
+      id: 1,
+      username: "bob",
+    }),
+  },
+  Mutation: {
+    login: (parent, { userInfo: { username } }, context, info) => {
+     // console.log(context)
+      return username;
+    },
+    register: () => ({
+      error: [
+        {
+          field: "username",
+          message: "bad",
+        },
+      ],
+      // error: null,
+      user: {
+        id: 1,
+        username: "bob",
+      },
+    }),
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+  context: ({req, res}) => ({req, res})
+});
 
-server.listen().then(({url}) => console.log(`server started at ${url}`))
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
